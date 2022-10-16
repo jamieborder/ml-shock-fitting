7ee93638a29a174c19e3346002f9c305a5da310a
Jamie Border <jamie.border@uq.net.au>
2022-10-17 00:16:59 +1000

    extracting all data and have added a template ffnn

--- post_process.py
+++ post_process.py
@@ -8,8 +8,15 @@ if len(sys.argv) < 2:
 
 # loading a number of data files in this folder
 # ns = [int(i) for i in sys.argv[1].split(',') if i != '']
-ns = [2]
+ns = [i for i in range(8)]
 ld = 'data_/'
+
+# plot_them = True
+plot_them = False
+
+params = np.empty([0,5])
+shapes = np.empty([0,41*2])
+coeffs = np.empty([0,7])
 
 for i,idx in enumerate(ns):
     # idx = int(sys.argv[-1])
@@ -23,8 +30,11 @@ for i,idx in enumerate(ns):
     M  = float(f.readline().strip().replace(' ','').split('=')[-1])
     T  = float(f.readline().strip().replace(' ','').split('=')[-1])
     dat = np.loadtxt(f)
+    params = np.vstack((params,np.array([[R2,K1,K2,M,T]])))
+    # shapes = np.vstack((shapes,dat.flatten()[None,:]))
+    shapes = np.vstack((shapes,dat.T.flatten()[None,:]))
 
-    # don't this residual is needed
+    # don't think this residual is needed
     # resf = f'res{idx:06d}.dat'
     # res = np.loadtxt(resf,skiprows=1)
 
@@ -41,8 +51,10 @@ for i,idx in enumerate(ns):
     # theta = arctan(y/x)
     # r = (x**2 + y**2)**0.5
 
-    theta = -np.arctan(dat[:,1] / dat[:,0])
+    theta = -np.arctan(dat[:,1] / (dat[:,0]-1e-16))
     r = (dat[:,0]**2 + dat[:,1]**2)**0.5
+    # plt.plot(theta,r,'k*-')
+    # plt.plot(dat[:,0],dat[:,1],'r.-')
 
     phi = 4.0 * theta / np.pi - 1.0
 
@@ -51,7 +63,7 @@ for i,idx in enumerate(ns):
     # 1, x, x^2, x^3, ...
     # f(theta=t) = sum_{k=0}^{M} c_k * t**k
 
-    M = 3
+    M = 6
 
     A = np.zeros((N,M+1))
     for k in range(A.shape[1]):
@@ -91,11 +103,16 @@ for i,idx in enumerate(ns):
         f += cs[k] * theta**k
         fc += csc[k] * theta**k
 
-    plt.plot(theta,f,'bo')
-    plt.plot(theta,r,'rs')
-        
+    if plot_them:
+        plt.plot(theta,f ,'bo' ,ms=12)
+        plt.plot(theta,fc,'rs' ,ms=12)
+        plt.plot(theta,r ,'k.-',ms=12)
+        print(np.sum(np.abs(f-r)),np.sum(np.abs(fc-r)))
+
     # STUFF IS WRONG! ALSO NEED TO CALCULATE BEZIER AND MAP THEM
     # TO THIS DOMAIN
 
+    coeffs = np.vstack((coeffs,cs.T))
 
-plt.show(block=False)
+if plot_them:
+    plt.show(block=False)
