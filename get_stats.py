14e97f7d5af070554d6ce7717cb9d8c7447e3e65
Jamie Border <jamie.border@uq.net.au>
2022-10-18 20:48:33 +1000

    can now look at processed data in get_stats

--- get_stats.py
+++ get_stats.py
@@ -3,9 +3,14 @@ import numpy as np
 import matplotlib.cm as cm
 from scipy.io import FortranFile
 from statistics import mean, median, stdev, variance, pstdev, pvariance
+import os
 
-paths = ['data/','data_/','data_1/','data_2/','data_3/','data_4/','data_6/','data_5/']
-ns = [i for i in range(100)]
+PROCESSED = os.getenv('PROCESSED')
+
+# paths = ['data/','data_/','data_1/','data_2/','data_3/','data_4/','data_6/','data_5/']
+# ns = [i for i in range(100)]
+paths = ['data_/']
+ns = [i for i in range(57)]
 
 
 R2s = []
@@ -14,23 +19,33 @@ K2s = []
 Ms = []
 Ts = []
 
-for path in paths:
-    for i,idx in enumerate(ns):
-        datf = path + f'run{idx:06d}.dat'
+if not PROCESSED:
 
-        f = open(datf,'r')
-        R2 = float(f.readline().strip().replace(' ','').split('=')[-1])
-        K1 = float(f.readline().strip().replace(' ','').split('=')[-1])
-        K2 = float(f.readline().strip().replace(' ','').split('=')[-1])
-        M  = float(f.readline().strip().replace(' ','').split('=')[-1])
-        T  = float(f.readline().strip().replace(' ','').split('=')[-1])
+    for path in paths:
+        print('path:',path)
+        for i,idx in enumerate(ns):
+            datf = path + f'run{idx:06d}.dat'
 
-        R2s.append(R2)
-        K1s.append(K1)
-        K2s.append(K2)
-        Ms.append(M)
-        Ts.append(T)
+            f = open(datf,'r')
+            R2 = float(f.readline().strip().replace(' ','').split('=')[-1])
+            K1 = float(f.readline().strip().replace(' ','').split('=')[-1])
+            K2 = float(f.readline().strip().replace(' ','').split('=')[-1])
+            M  = float(f.readline().strip().replace(' ','').split('=')[-1])
+            T  = float(f.readline().strip().replace(' ','').split('=')[-1])
 
+            R2s.append(R2)
+            K1s.append(K1)
+            K2s.append(K2)
+            Ms.append(M)
+            Ts.append(T)
+
+else:
+    dat = np.loadtxt('runs/data/params.dat')
+    R2s = dat[:,0]
+    K1s = dat[:,1]
+    K2s = dat[:,2]
+    Ms = dat[:,3]
+    Ts = dat[:,4]
 
 print(f'some statistics;')
 print(f'    total number of samples: {len(R2s)}')
@@ -38,3 +53,11 @@ for name,dat in zip(['R2','K1','K2','M','T'],[R2s,K1s,K2s,Ms,Ts]):
     print(f'    {name}:')
     print(f'        max, min, mean, median : {min(dat):6f}, {max(dat):6f}, {mean(dat):6f}, {median(dat):6f}')
     print(f'        std-deviation, variance: {pstdev(dat):6f}, {pvariance(dat):6f}')
+
+plt.figure(1)
+plt.plot(R2s,K1s,'bo')
+plt.plot(R2s,K2s,'rs')
+# plt.plot(K1s,K2s,'rs')
+# plt.plot(R2s,Ms,'g*')
+# plt.plot(R2s,Ts,'m*')
+plt.show()
